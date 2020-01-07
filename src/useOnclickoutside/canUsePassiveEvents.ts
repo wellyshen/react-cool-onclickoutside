97b19b691a5e28bdfa2fb2ad6e481f146bf8f4eb
Welly Shen <hivoid19@gmail.com>
2020-01-07 16:27:16 +0800

    Feat: add detect passive events util

---
+++ src/useOnclickoutside/canUsePassiveEvents.ts
@@ -0,0 +1,22 @@
+export default (): boolean => {
+  if (
+    typeof window === 'undefined' ||
+    typeof window.addEventListener !== 'function'
+  )
+    return false;
+
+  let passive = false;
+  const options = Object.defineProperty({}, 'passive', {
+    // eslint-disable-next-line getter-return
+    get() {
+      passive = true;
+    }
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const noop = (): void => {};
+  window.addEventListener('test', noop, options);
+  window.removeEventListener('test', noop, options);
+
+  return passive;
+};
