d24eba8fbe3d982d6d742ea60a397dc68636c7bc
Welly Shen <hivoid19@gmail.com>
2020-01-07 15:23:48 +0800

    Feat: implement main logic

--- src/useOnclickoutside/index.ts
+++ src/useOnclickoutside/index.ts
@@ -1,5 +1,39 @@
-export default (): string => {
-  // ...
+/* eslint-disable consistent-return */
 
-  return 'Coming soon...';
+import { RefObject, useRef, useCallback, useEffect } from 'react';
+
+export default (
+  cb: (event?: MouseEvent | TouchEvent) => void,
+  events: string[] = ['mousedown', 'touchstart']
+): RefObject<HTMLElement | null> | void => {
+  if (typeof document === 'undefined' || !document.createElement) return;
+
+  const ref = useRef();
+
+  const handler = useCallback(
+    e => {
+      const { current } = ref;
+
+      // @ts-ignore
+      if (current && !current.contains(e.target) && cb) cb(e);
+    },
+    [ref, cb]
+  );
+
+  useEffect(() => {
+    if (!cb) return;
+
+    events.forEach(e => {
+      document.addEventListener(e, handler);
+    });
+
+    return (): void => {
+      events.forEach(e => {
+        document.removeEventListener(e, handler);
+      });
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cb, events]);
+
+  return ref;
 };
