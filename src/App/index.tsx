022e4a3fbbd9711899c15cb3deab752949d6b5b9
Welly Shen <hivoid19@gmail.com>
2020-01-08 11:01:05 +0800

    Chore: remove un-used dep

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -14,6 +14,8 @@ const App: SFC<{}> = () => {
   const setRef = useOnclickoutside(() => {
     setTxt1('Clicked outside');
     setTxt2('Clicked outside');
+
+    console.log('LOG ===> ', Math.random());
   });
 
   const handleArea1 = (): void => {
